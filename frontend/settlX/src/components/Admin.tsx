e735eabe1a5277e1d274afec69968c2ec61b44d2
chigozie0706 <chigoziejacob@gmail.com>
2026-02-17 12:56:02 +0100

    removed nested git

--- frontend/settlX/src/components/Admin.tsx
+++ frontend/settlX/src/components/Admin.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect, useState } from "react";
 import { useAccount, useReadContract, useWriteContract } from "wagmi";
 import contractABI from "../contracts/settlX.json";
